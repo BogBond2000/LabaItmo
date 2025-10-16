50752c189092c8457c9427b1715e848c4e41c015
BogBond2000 <111767325+BogBond2000@users.noreply.github.com>
2025-10-16 12:32:21 +0300

    Create fp1.exs

---
+++ fp1.exs
@@ -0,0 +1,18 @@
+defmodule MyProject do
+  def sum_square() do
+    Enum.reduce(1..100,0,&+/2) ** 2
+  end
+  def square_sum() do
+    1..100 |> Enum.map(&(&1*&1)) |> Enum.reduce(0,&+/2)
+  end
+
+  def fibonachi(n) when n > 0 do
+      fib_help(n,1,0)
+  end
+  defp fib_help(n,a,b) when n==1 do
+    a
+  end
+  defp fib_help(n,a,b) do
+    fib_help(n-1,a+b,a)
+  end
+end
