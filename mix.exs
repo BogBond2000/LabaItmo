3582d02c940586cd71b0b7a115e7347e00659d62
BogBond2000 <111767325+BogBond2000@users.noreply.github.com>
2025-10-28 19:55:31 +0300

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule MyProjec.MixProject do
     [
       app: :my_projec,
       version: "0.1.0",
-      elixir: "~> 1.18",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
